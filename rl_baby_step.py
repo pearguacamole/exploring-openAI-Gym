cfee1f5ea4b993307b8a3fa38aa8c75a5e3a15ad
pear <pearguacamole40zr@gmail.com>
2024-06-02 01:20:35 +0530

    Implement RL and maintain results

--- rl_baby_step.py
+++ rl_baby_step.py
@@ -4,7 +4,23 @@ import tensorflow
 #import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten
-from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.optimizers.legacy import Adam
+
+
+class GymWrapper(gym.Wrapper):
+    def __init__(self, env):
+        gym.Wrapper.__init__(self, env)
+        self.env = env
+
+    def reset(self, **kwargs):
+        obs, _ = self.env.reset(**kwargs)
+        return obs
+
+    def step(self, action):
+        obs, reward, done, info, _ = self.env.step(action)
+        return obs, reward, done, {}
+
+
 
 #this solves "ImportError: cannot import name '__version__' from 'tensorflow.keras'" from rl.agents import
 from keras import __version__
@@ -16,11 +32,13 @@ from rl.policy import BoltzmannQPolicy
 from rl.memory import SequentialMemory
 
 #env = gym.make("CartPole-v1", render_mode = 'human')
-env = gym.make("CartPole-v1")
+#env = gym.make("CartPole-v1")
+env = GymWrapper(gym.make("CartPole-v1"))
 
 
 states = env.observation_space.shape[0]
 actions = env.action_space.n
+
 
 model = Sequential()
 model.add(Flatten(input_shape=(1, states))) 
@@ -38,9 +56,10 @@ agent = DQNAgent(
 )
 
 agent.compile(Adam(lr=0.001),metrics=['mae'])
-agent.fit(env,nb_steps=10000,visualize=False,verbose=1)
-
+agent.fit(env,nb_steps=10000,visualize=False,verbose=2)
+env = GymWrapper(gym.make("CartPole-v1",render_mode='human'))
 results = agent.test(env,nb_episodes=10,visualize=True)
+print(results.history['episode_reward'])
 
 # episodes =10
 # for episode in range(1,episodes+1):
